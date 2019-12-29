e26eb9b6eed812724705f00fc0b17a6e80e49d5c
D Bruce Saurer <notami@gmail.com>
2019-12-28 22:38:00 -0500

    poly on both monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-polybar default&
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
-echo "Bar launched"
+echo "Bars launched"
 
